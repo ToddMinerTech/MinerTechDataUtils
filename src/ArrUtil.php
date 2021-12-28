e4f9b6493b0a9ea7e9481173268ce72c0b77c3e4
Todd Miner <todd@miner.technology>
2021-12-27 16:34:28 -0800

    Added new match/map value in object array

--- src/ArrUtil.php
+++ src/ArrUtil.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace ToddMinerTech\DataUtils;
 
+use ToddMinerTech\DataUtils\StringUtil;
+
 /**
  * Class ArrUtil
  *
@@ -28,5 +30,56 @@ class ArrUtil
             $inputArr[] = $newValue;
         }
         return $inputArr;
+    }
+    
+    /**
+     * matchValueInObjectArray
+     *
+     * Searches and returns an object from within an array using defined criteria.
+     * 
+     * @param string $valueToMatch The value you are trying to match within the object attribute
+     * 
+     * @param string $attributeToMatch The attribute name you want to check the value of
+     * 
+     * @param array $arrayToSearch The array of objects you are trying to match
+     *
+     * @return object Returns the object if matched, or null if no match is found
+     */
+    public static function matchValueInObjectArray(string $valueToMatch, string $attributeToMatch, array $arrayToSearch): ?object
+    {
+        for($i = 0; $i < count($arrayToSearch); $i++) {
+            if(!isset($arrayToSearch[$i]->$attributeToMatch)) {
+                continue;
+            }
+            if(StringUtil::sSComp($arrayToSearch[$i]->$attributeToMatch, $valueToMatch)) {
+                return $arrayToSearch[$i];
+            }
+        }
+        return null;
+    }
+    
+
+    /**
+     * mapValueInObjectArray
+     *
+     * Wraps matchValueInObjectArray to provide a specific mapped output value from search criteria and array of objects.
+     * 
+     * @param string $valueToMatch The value you are trying to match within the object attribute
+     * 
+     * @param string $attributeToMatch The attribute name you want to check the value of
+     * 
+     * @param string $attributeToReturn The attribute name you want to return from the matched object
+     * 
+     * @param array $arrayToSearch The array of objects you are trying to match
+     *
+     * @return string Returns the mapped value if a match is found, otherwise returns the input value unchanged
+     */
+    public static function mapValueInObjectArray(string $valueToMatch, string $attributeToMatch, string $attributeToReturn, array $arrayToSearch): string
+    {
+        $matchedObject = self::matchValueInObjectArray($valueToMatch, $attributeToMatch, $arrayToSearch);
+        if(!$matchedObject || !isset($matchedObject->$attributeToReturn)) {
+            return $valueToMatch;
+        }
+        return $matchedObject->$attributeToReturn;
     }
 }
